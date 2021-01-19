603bc876fda3071ff2d0ba4f6120e5e484fe1bfd
Ben Scheirman <ben@scheirman.com>
2021-01-19 12:58:50 -0600

    Episode 472

---
+++ PortalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PortalApp
+//
+//  Created by Ben Scheirman on 1/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
